03f9f86c5e8f27c33f0eba4e23ad992b805a59e7
dina16in-lab <dina16in@gmail.com>
2026-07-21 11:02:56 +0530

    Create leetcode 1768.java

---
+++ leetcode 1768.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static String mergeAlternately(String word1, String word2) {
+
+        StringBuilder result = new StringBuilder();
+
+        int i = 0;
+
+        while (i < word1.length() || i < word2.length()) {
+
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
+            }
+
+            if (i < word2.length()) {
+                result.append(word2.charAt(i));
+            }
+
+            i++;
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String word1 = "abc";
+        String word2 = "pqr";
+
+        String answer = mergeAlternately(word1, word2);
+
+        System.out.println("Merged String: " + answer);
+    }
+}
